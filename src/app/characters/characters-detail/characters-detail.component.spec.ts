438cbefcde14d8b86f276b3cd7c58e9daa902314
Rafael Paes de Almeida <rafaelpaes@inatel.br>
2018-10-26 10:44:17 -0300

    Project updated

--- src/app/characters/characters-detail/characters-detail.component.spec.ts
+++ src/app/characters/characters-detail/characters-detail.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideRoutes, Routes, RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
 
 import { CharactersDetailComponent } from './characters-detail.component';
+
+let config: Routes = [
+  {
+      path: '', component: CharactersDetailComponent
+  }
+];
 
 describe('CharactersDetailComponent', () => {
   let component: CharactersDetailComponent;
@@ -8,7 +17,9 @@ describe('CharactersDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CharactersDetailComponent]
+      imports: [RouterTestingModule, RouterModule ],
+      declarations: [  ],
+      providers: [  ]
     })
       .compileComponents();
   }));
@@ -17,9 +28,5 @@ describe('CharactersDetailComponent', () => {
     fixture = TestBed.createComponent(CharactersDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
--- src/app/characters/characters-list/characters.list.component.spec.ts
+++ src/app/characters/characters-list/characters.list.component.spec.ts
@@ -18,8 +18,4 @@ describe('CharactersListComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
